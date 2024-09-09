2f68ee88b51f33508c7d8f81212a600968335233
Darr Or <darrmorgan@gmail.com>
2024-09-09 16:49:44 -0400

    Connect to atlas data

--- config/index.js
+++ config/index.js
@@ -14,6 +14,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 // config.isGuestMode = true
 
-// config = configProd
+config = configProd
 
 
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://darrmorgan:<ilanOr77>@cluster0.pxhqzar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  dbName: process.env.DB_NAME || 'station_db'
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://darrmorgan:ilanOr77@cluster0.pxhqzar.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  dbName: process.env.DB_NAME || 'sprintify_db'
 }
